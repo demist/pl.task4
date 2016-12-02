92d760f10e5c0a96fbe6c5d35f21fcb251fb52ad
Nikita Starichkov <nikita.starichkov@gmail.com>
2016-12-02 12:52:03 +0300

    game methods added

--- Server.java
+++ Server.java
@@ -104,14 +104,106 @@ public class Server
 	private static class GameThread extends Thread
 	{
 	    private ArrayList<Player> players;
+	    private int lastInGame = -1;
+	    private ArrayList<ArrayList<Integer>> game;
 		public GameThread(ArrayList<Player> players)
 		{
 		  this.players = players;
 		}
 
+		private void newGame()
+		{
+			game = new ArrayList<ArrayList<Integer>>(20); // 20 - start size
+			//preparing game field
+			for (int i = 0; i < game.size(); i++)
+			{
+				ArrayList<Integer> inner = new ArrayList<Integer>(game.size());
+				for (int k = 0; k < inner.size(); k++)
+					inner.set(k, -1);
+				game.set(i, inner);
+			}
+		}
+
+		private boolean checkFive(int a, int b, int c, int d, int e)
+    	{
+      		if (a == -1)
+        		return false;
+      		if (a != b)
+        		return false;
+      		if (a != c)
+        		return false;
+      		if (a != d)
+        		return false;
+      		if (a != e)
+        		return false;
+      		return true;
+    	}
+
+		//very simple, we can make lazy check with knowledge of the last turn
+    	private boolean checkWin()
+    	{
+      		//horizontal case _
+      		for (int i = 0; i < game.size(); i++)
+      		{
+        		ArrayList<Integer> cur = game.get(i);
+        		for (int j = 0; j < cur.size() - 4; j++)
+        		{
+          			if (checkFive(cur.get(j), cur.get(j + 1), cur.get(j + 2), cur.get(j + 3), cur.get(j + 4)))
+            			return true;
+        		}
+      		}
+      		//vertical case |
+      		for (int i = 0; i < game.size() - 4; i++)
+      		{
+        		for (int j = 0; j < game.size(); j++)
+        		{
+          			if (checkFive(game.get(i).get(j), game.get(i + 1).get(j), game.get(i + 2).get(j), game.get(i + 3).get(j), game.get(i + 4).get(j)))
+            			return true;
+        		}
+      		}
+      		//diagonal case 1 /
+      		for (int i = 0; i < game.size() - 4; i++)
+      		{
+        		for (int j = 0; j < game.size() - 4; j++)
+        		{
+          			if (checkFive(game.get(i + 4).get(j), game.get(i + 3).get(j + 1), game.get(i + 2).get(j + 2), game.get(i + 1).get(j + 3), game.get(i).get(j + 4)))
+            			return true;
+        		}
+      		}
+      		//diagonal case 2 \
+      		for (int i = 0; i < game.size() - 4; i++)
+      		{
+        		for (int j = 0; j < game.size() - 4; j++)
+        		{
+          			if (checkFive(game.get(i).get(j), game.get(i + 1).get(j + 1), game.get(i + 2).get(j + 2), game.get(i + 3).get(j + 3), game.get(i + 4).get(j + 4)))
+            			return true;
+        		}
+      		}
+      		//default
+      		return false;
+    	}
+
+    	private void nextTurn(int id, int i, int j)
+    	{
+      		ArrayList<Integer> tmp = game.get(i);
+      		tmp.set(j, id);
+      		//here we can add some code to expanse game field 
+    	}
+
 		public void run()
 		{
-		
+			//waiting for 2 players min to start the game
+			if (players.size() < 2)
+			{
+				try
+				{
+					Thread.sleep(1000);
+				}
+				catch (Exception e)
+				{
+					System.out.println(e);
+				}
+			}
 		}
 	}
 
